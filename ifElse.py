9234556e70551ca37641f76719e30748e4d53e52
Nicolas <nklange986@gmail.com>
2025-04-10 17:09:09 -0300

    operadores para calculcos

--- ifElse.py
+++ ifElse.py
@@ -3,6 +3,6 @@ nome = input("Informe o seu nome: ")
 idade = int(input("Informe a sua idade: "))
 
 if idade >= 18:
-    print(nome + " você é maior de idade")
+    print(nome, " você é maior de idade")
 else:
-    print(nome + " você é menor de idade")
+    print(nome, " você é menor de idade")
---
+++ operadores.py
@@ -0,0 +1,11 @@
+soma = 1 + 1
+subtracao = 10 - 5
+multiplicacao = 5 * 5
+divisao = 10 / 2
+potencia = 7 ** 2
+
+print("Soma ", soma)
+print("Subtração ", subtracao)
+print("Multiplcação ", multiplicacao)
+print("Divisão ", divisao)
+print("Potência ", potencia)
