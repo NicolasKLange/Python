79aca88426b84ec9161507cd388125b4de70fb38
Nicolas <nklange986@gmail.com>
2025-04-14 17:55:47 -0300

    range

---
+++ range.py
@@ -0,0 +1,8 @@
+# Repetindo o for de acorddo com a quantidade do range
+# Neste caso está mostrando os números de 1 a 10
+# Por convensão e sugestão, muitas pessoas utilizam
+# o _ para esse for, pois essa variável é meio que inútil
+# para o resto do código, mais pode colocar qualquer nome.
+
+for i in range(10):
+    print("N°", i + 1)
