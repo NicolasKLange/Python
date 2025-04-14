c76ffc0390696c32152aba9f77cc0315f2f55dd3
Nicolas <nklange986@gmail.com>
2025-04-14 18:00:19 -0300

    match case ´para verificar dia da semana

---
+++ matchCase.py
@@ -0,0 +1,22 @@
+# Verificando o dia da semana
+dia = 1
+
+# March case para fazer a verificação do numero ao dia a semana
+match dia:
+
+    case 1:
+        print("Domingo")
+    case 2:
+        print("Segunda")
+    case 3:
+        print("Terça")
+    case 4:
+        print("Quarta")
+    case 5:
+        print("Quinta")
+    case 6:
+        print("Sexta")
+    case 7:
+        print("Sábado")
+    case _:
+        print("Digite um número correspondente")
